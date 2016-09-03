56dc5197a46f2778d5cce6deb8d11bed5b521bc8
edgardolopez <edgardo.skl@gmail.com>
2016-09-03 18:38:26 -0300

    Agrego interface Queue

---
+++ src/main/java/ar/fiuba/tdd/template/Queue.java
@@ -0,0 +1,9 @@
+package ar.fiuba.tdd.template;
+
+interface Queue<T> {
+    boolean isEmpty();
+    int size();
+    void add(T item); // agregar un item
+    T top(); // retornar el primer item, lanzar exception si esta vacío.
+    void remove(); // remover el primer item lanzar exception si esta vacío.
+}
